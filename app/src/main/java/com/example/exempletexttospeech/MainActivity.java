ecbca56cd2b616537cff6f885b40405c5b5ba75a
Yoann Kergall <yoann.kergall@gmail.com>
2020-04-15 12:21:02 +0200

    ajout du choix de la langue

--- app/src/main/java/com/example/exempletexttospeech/MainActivity.java
+++ app/src/main/java/com/example/exempletexttospeech/MainActivity.java
@@ -6,10 +6,14 @@ import android.content.Context;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = this;
-        initTextToSpeech("");
+
+        initSpinnerLangues();
+        initTextToSpeech(0);
 
         final EditText txtMessage = findViewById(R.id.txtMessage);
         Button btnMessageParle = findViewById(R.id.btnMessageParle);
@@ -38,18 +44,53 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 textToSpeech.speak(message, TextToSpeech.QUEUE_FLUSH, null, "");
-
             }
         });
     }
 
-    private void initTextToSpeech(String langue) {
+    private void initTextToSpeech(final int langue) {
         textToSpeech = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if (status == TextToSpeech.SUCCESS) {
-                    textToSpeech.setLanguage(Locale.FRANCE);
+
+                    switch (langue) {
+                        case 1:
+                            textToSpeech.setLanguage(Locale.FRANCE);
+                            break;
+                        case 2:
+                            textToSpeech.setLanguage(Locale.ENGLISH);
+                            break;
+                        default:
+                            textToSpeech.setLanguage(Locale.FRANCE);
+                            break;
+                    }
+
                 }
+            }
+        });
+    }
+
+    private void initSpinnerLangues() {
+        Spinner sprLangues = findViewById(R.id.sprLangues);
+
+        ArrayList<String> langues = new ArrayList<>();
+        langues.add(0, "Sélectionnez la langue");
+        langues.add(1, "Français");
+        langues.add(2, "Anglais");
+
+        ArrayAdapter adapter = new ArrayAdapter(context, android.R.layout.simple_spinner_item, langues);
+        sprLangues.setAdapter(adapter);
+
+        sprLangues.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                initTextToSpeech(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
     }
